e5977343d38197fd748d9fa222c565067eb31d82
andrzejlisek <andrzejlisek@poczta.onet.pl>
2023-03-13 15:28:09 +0100

    X

--- HtmlBundle/App2.js
+++ HtmlBundle/App2.js
@@ -1,6 +1,20 @@
 import ModFun from "./Mod.js";
 
-function MulDiv() {
+function MulDiv1() {
+    let Num1 = parseInt(document.querySelector('[id=\'Num1\']').value);
+    let Num2 = parseInt(document.querySelector('[id=\'Num2\']').value);
+    let NumResult = ModFun(Num1, Num2);
+    document.querySelector('[id=\'NumResult2\']').innerHTML = NumResult;
+}
+
+export function MulDiv2() {
+    let Num1 = parseInt(document.querySelector('[id=\'Num1\']').value);
+    let Num2 = parseInt(document.querySelector('[id=\'Num2\']').value);
+    let NumResult = ModFun(Num1, Num2);
+    document.querySelector('[id=\'NumResult2\']').innerHTML = NumResult;
+}
+
+const MulDiv3 = () -> {
     let Num1 = parseInt(document.querySelector('[id=\'Num1\']').value);
     let Num2 = parseInt(document.querySelector('[id=\'Num2\']').value);
     let NumResult = ModFun(Num1, Num2);
